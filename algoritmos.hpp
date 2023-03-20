ea126874af6b4c8cbd5c01cf34f8b07910fa2ba3
Pedro Antonio Mayorgas Parejo <pedroamp@correo.ugr.es>
2023-03-20 18:24:27 +0100

    Inicio de Practica1

---
+++ algoritmos.hpp
@@ -0,0 +1,163 @@
+#include <vector>
+
+using namespace std;
+
+/*Algoritmos iterativos*/
+
+/*
+    Primer ejercicio: Dado un vector de N elementos repetidos, retornar otro, que no esté repetido.7
+    Eficiencia teorica N^2
+    OK
+*/
+void vectorSinRepeticion(const vector<int> &vectorOriginal, vector<int> &vectorFinal, const int N)
+{
+    // Booleano controlador de encontrar repetidos
+    bool encontrado = false;
+    int tamanioVector = vectorFinal.size();
+    // Caso Base, aniadimos el primer elemento, ya que ese nunca va a estar repetido
+    vectorFinal.push_back(vectorOriginal[0]);
+
+    for (int i = 1; i < N; i++)
+    {
+        // Bucle de recorrido interno, para buscar el duplicado.
+        for (int j = 0; j < tamanioVector && !encontrado; j++)
+        {
+            // Encontramos un duplicado, nos salimos del bucle.
+            if (vectorFinal[j] == vectorOriginal[i])
+            {
+                encontrado = true;
+            }
+        }
+
+        // Hemos encontrado un elemento en el vectorFinal, aniadimos el elemento del vectorOriginal
+        if (!encontrado)
+        {
+            vectorFinal.push_back(vectorOriginal[i]);
+            tamanioVector = vectorFinal.size();
+        }
+        else
+        {
+            encontrado = false;
+        }
+    }
+}
+
+void merge(vector<int> &unorderedVector, const int left, const int medio, const int right)
+{
+    // Iteradores, no coge el final, es un rango [left, medio), [medio, right)
+    cout << "Falla" << endl;
+    vector<int>::const_iterator begin_array1 = unorderedVector.begin() + left;
+    vector<int>::const_iterator end_array1_begin_array2 = unorderedVector.begin() + medio;
+    vector<int>::const_iterator end_array2 = unorderedVector.begin() + right;
+    // Creamos los arrays
+    cout << "Precopia" << endl;
+    vector<int> array1(begin_array1, end_array1_begin_array2);
+    vector<int> array2(end_array1_begin_array2, end_array2);
+    cout << "PostCopia" << endl;
+    for (int i = 0; i < array1.size(); i++){
+        cout << array1[i] << " ";
+    }
+    cout << "Fin array 1" << endl;
+
+
+    for (int i = 0; i < array2.size(); i++){
+        cout << array2[i] << " ";
+    }
+    cout << "Fin array 2" << endl;
+
+    cout << "Fin recursividad" << endl;
+
+       for (int i = left; i < right; i++){
+        cout << unorderedVector[i] << " ";
+     }
+     cout << "Fin unordered vector" << endl;
+
+    // Averiguamos los tamanios de los subarrays
+    int sizeofarray1 = array1.size();
+    int sizeofarray2 = array2.size();
+
+    // Empezamos a mezclar
+    int i = 0;
+    int j = 0;
+    // K es el principio del array desordenado con DyQ
+    int k = left;
+
+    // Recogemos los elementos mas pequenios entre los dos array hasta que uno de los iteradores i j termine.
+    while (i < sizeofarray1 && j < sizeofarray2)
+    {
+        if (array1[i] <= array2[j])
+        {
+            unorderedVector[k] = array1[i];
+            i++;
+        }
+        else
+        {
+            unorderedVector[k] = array2[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Recogemos los elementos restantes, que son mas grandes
+    while (i < sizeofarray1)
+    {
+        unorderedVector[k] = array1[i];
+        i++;
+        k++;
+    }
+
+    while (j < sizeofarray2)
+    {
+        unorderedVector[k] = array2[j];
+        j++;
+        k++;
+    }
+
+    cout << endl << "VECTOR ORDENADO " << endl ; 
+    for (int i = left; i < right; i++){
+        cout << unorderedVector[i] << " ";
+     }
+     cout << endl ;
+}
+
+void mergeSort(vector<int> &unorderedVector, const int left, const int right)
+{
+    // punto medio
+    // int medio = (left + (right - left)) / 2;
+    int medio = (left+right) / 2;
+    // 1 + (2 - 1) / 2 = 2 / 2 = 1  l == m
+
+    if (medio == left){
+        return;
+    }
+
+    if (left < right)
+    {
+    mergeSort(unorderedVector, left, medio);
+    mergeSort(unorderedVector, medio , right);
+    merge(unorderedVector, left, medio, right);
+    }
+}
+
+/*
+    El algoritmo
+
+*/
+void repetidosEficienteOrdenado(vector<int> &vectorOriginal, vector<int> &vectorFinal, const int N)
+{
+    // Ordenamos el vector con el algoritmo mas eficiente posible
+    // mergeSort(vectorOriginal, 0, N);
+
+    // EJE 2
+    // Cogemos de los elementos ordenados, los que no se repitan mas de una vez
+    int actual = -1;
+
+    for (int i = 0; i < vectorOriginal.size(); i++)
+    {
+        if (actual != vectorOriginal[i])
+        {
+            actual = vectorOriginal[i];
+            vectorFinal.push_back(actual);
+        }
+    }
+}
---
+++ practica1algoritmos.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "algoritmos.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <fstream>
+using namespace std ;
+
+
+int main(int argc, char *argv[]){
+	/* Vectores de entrada de prueba del ejercicio*/
+	// 5 1 9 2 2 5 1 1 7
+	// 1 1 1 2 2 5 5 7 9
+	vector<int> ejercicio1 = {5,1,9,2,2,5,1,1,7};
+    vector<int> vectorFinal ;
+	vector<int> ejercicio2 = {1,1,1,2,2,5,5,7,9};
+    int *v;
+	int n, i, argumento;
+    chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
+	double tejecucion; // tiempo de ejecucion del algoritmo en ms
+	unsigned long int semilla;
+	ofstream fsalida;
+	
+	// if (argc <= 3) {
+	// 	cerr<<"\nError: El programa se debe ejecutar de la siguiente forma.\n\n";
+	// 	cerr<<argv[0]<<" NombreFicheroSalida Semilla tamCaso1 tamCaso2 ... tamCasoN\n\n";
+	// 	return 0;
+	// }
+	
+	// Abrimos fichero de salida
+	// fsalida.open(argv[1]);
+	// if (!fsalida.is_open()) {
+	// 	cerr<<"Error: No se pudo abrir fichero para escritura "<<argv[1]<<"\n\n";
+	// 	return 0;
+	// }
+	
+	// Inicializamos generador de no. aleatorios
+	// semilla= atoi(argv[2]);
+	// srand(semilla);
+	
+	// Pasamos por cada tamanio de caso para el ejercicio 1
+	// for (argumento= 3; argumento<argc; argumento++) {
+		
+		// Cogemos el tamanio del caso
+		// n= atoi(argv[argumento]);
+		
+		// Reservamos memoria para el vector
+		// v= new int[n];
+		
+		// Generamos vector aleatorio de prueba, con componentes entre 0 y n-1
+		// for (i= 0; i<n; i++)
+		// 	v[i]= rand()%n;
+		cout << ejercicio1.size() << endl ;
+
+		mergeSort(ejercicio1, 0, ejercicio1.size());
+		for (int i = 0; i < ejercicio1.size(); i++)
+			cout << ejercicio1[i] << " ";
+        cout << endl ;
+
+		cerr << "Ejecutando algoritmo ejercicio 1 para tam " << n << endl ;
+
+		t0= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que comienza la ejecuciÛn del algoritmo
+        //Ejecutamos algoritmo ejercicio 1
+        // vectorSinRepeticion(ejercicio1, vectorFinal, ejercicio1.size());
+		repetidosEficienteOrdenado(ejercicio1, vectorFinal, ejercicio2.size());
+
+		tf= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que finaliza la ejecuciÛn del algoritmo
+
+        cerr << "\tTiempo de ejec. (us): " << tejecucion << " para tam. caso "<< n<<endl;
+
+		for (int i = 0; i < vectorFinal.size(); i++)
+			cout << vectorFinal[i] << " ";
+        cout << endl ;
+
+        // Guardamos tam. de caso y t_ejecucion a fichero de salida
+		// fsalida<<n<<" "<<tejecucion<<"\n";
+		
+		
+		// Liberamos memoria del vector
+		// delete [] v;
+    // }
+    // Cerramos fichero de salida
+	// fsalida.close();
+}
